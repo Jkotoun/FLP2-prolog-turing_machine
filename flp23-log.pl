44e73a897f2a04d128b8366c159b50c4fabae808
Jkotoun <jkotoun58@gmail.com>
2023-04-16 01:21:46 +0200

    step wip

--- flp23-log.pl
+++ flp23-log.pl
@@ -1,6 +1,6 @@
 :- dynamic rule/4 as rule.
 
-:- functor(tsconfig(left, state, head, right), tsconfig, 4).
+% :- functor(tsconfig(left, state, head, right), tsconfig, 4).
 
 
 %used from example
@@ -9,43 +9,49 @@ split_line([' '|T], [[]|S1]) :- !, split_line(T,S1).
 split_line([32|T], [[]|S1]) :- !, split_line(T,S1).   
 split_line([H|T], [[H|G]|S1]) :- split_line(T,[G|S1]).
 
-atom_codes_list_to_atoms_list([], []).
-atom_codes_list_to_atoms_list([H|T], [AtomH|AtomT]) :-
-    atom_codes(AtomH, H),
-    atom_codes_list_to_atoms_list(T, AtomT).
 
+codes_to_atoms([], []).
+codes_to_atoms([Code|Codes], [Atom|Atoms]) :-
+    atom_codes(Atom, [Code]),
+    codes_to_atoms(Codes, Atoms).
 
-
-read_input(TSConfig) :-
+read_input(tsconfig(Left, State, Head, Right)) :-
      read_line_to_codes(user_input, Line),
      (at_end_of_stream ->
-        split_line(Line, Parts),
-        maplist(atom_codes, AtomParts, Parts),
+        codes_to_atoms(Line, Atoms),
         Left = [],
         State = 'S',
-        nth0(0, AtomParts, Head),
-        Right = AtomParts,
-        TSConfig = tsconfig(Left, State, Head, Right)
+        nth0(0, Atoms, Head),
+        Right = Atoms
         ;
         split_line(Line, Parts),
         maplist(atom_codes, AtomParts, Parts),
         Rule =.. [rule | AtomParts],
         assertz(Rule),
-        read_input(TSConfig)
+        read_input(tsconfig(Left, State, Head, Right))
      ).
 % [State, HeadSymbol, NewState, NewHeadSymbol]
+
+
+step(tsconfig(Left, State, Head, Right), tsconfig(NextLeft, NextState, NextHead, NextRight)).
+
+accepting(tsconfig(_, State, _, _)) :- State == 'F'.
+accepting(TSConfig) :- step(TSConfig, NextTSConfig), accepting(NextTSConfig).
+
+
+
 
 print_rules :- 
     rule(X, Y, Z, W),
     format("~w ~w ~w ~w", [X, Y, Z, W]),
     nl,
-
     fail.
 
 start :-
     read_input(TSConfig),
-    writeln("config:"),
     writeln(TSConfig),
+    (accepting(TSConfig) -> writeln("True") ; writeln("False")),
+    
     writeln("rules:"),
     
     print_rules -> true ; true,
